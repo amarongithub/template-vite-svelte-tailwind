c665d3a58e3b8e5ba4cbe571dc70eade7f121ebc
AmarOnGitHub <31379185+amarongithub@users.noreply.github.com>
2023-12-12 12:08:38 +0530

    Made changes for default page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,15 @@
 <script>
   let count = 0;
-  
-  function increment() {
-    count ++;
-  }
-</sript>
 
-{count}
-<button on:click={increment}>Click</button>
+  function increment() {
+    count++;
+  }
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+  <p class="text-2xl font-bold">{count}</p>
+  <button
+    class="mt-4 rounded-lg bg-purple-900 px-4 py-2 text-white"
+    on:click={increment}>Click</button
+  >
+</div>
